fe1c0fc1ae71c08ea9c5d5168eb47cb7281f2711
Thiago Paz <programador@programador.me>
2021-03-20 09:49:07 +0900

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,27 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(newTaskTitle.length > 0) {
+      setTasks([...tasks, {
+        id: Math.floor(Math.random() * 9999) + 1  ,
+        title: newTaskTitle, 
+        isComplete:false
+      }]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map((task) => {
+      if(task.id === id) {
+        task.isComplete = true
+      }
+      return task;
+    }));
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task)=> task.id !== id))
   }
 
   return (
